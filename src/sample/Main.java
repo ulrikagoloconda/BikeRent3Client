c5149494dbab719467f99ce6bad25a6d8d62b9f0
ulrikagoloconda <golocondafahlen@gmail.com>
2016-11-24 19:53:47 +0100

    Commit settings

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
