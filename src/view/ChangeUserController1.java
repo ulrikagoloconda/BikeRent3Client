a868cdd0c12359854e20f0b577fda626b63ef97e
cykeltur <cykeltur@gmail.com>
2016-12-12 15:44:26 +0100

    added update and loginUser

--- src/view/ChangeUserController1.java
+++ src/view/ChangeUserController1.java
@@ -1,16 +1,14 @@
 package view;
 
-import model.BikeUser;
+import ServerConnecttion.ServerCallImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import mailing.SentMail;
+import model.BikeUser;
 
-import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 /**
@@ -22,6 +20,7 @@ public class ChangeUserController1 implements Initializable {
     private BikeUser currentUser;
     private String errorTitle;
     private Integer userID;
+  private ServerCallImpl serverCall;
     @FXML
     private TextField userNameText, fNameText,lNameText,mailText, phoneText, passwordText, passwordCheckerText;
     @FXML
@@ -34,6 +33,7 @@ public class ChangeUserController1 implements Initializable {
         errorTitle = "fel i uppdatera användare";
         userID = currentUser.getUserID();
         populateText();
+      serverCall = new ServerCallImpl();
 
     }
 
@@ -58,27 +58,26 @@ public class ChangeUserController1 implements Initializable {
         String passwordChecker = passwordCheckerText.getText();
         phoneString.replace("-", "");
         phoneString.replace("+", "");
-      /*  try {
-            if (userName.length() < 5) {
+
+      boolean isAddUserOK = false;
+      if (userName.length() < 5) {
                 System.out.println("username to short");
-                //  ErrorView.showError(errorTitle, "fel vid uppdatering", "Kontrollera era uppgifter", userID, new IOException("username is to short!"));
+                  ErrorView.showError(errorTitle, "fel vid uppdatering", "Kontrollera era uppgifter", userID, new Exception("username is to short!"));
             } else {
-                int phone = Integer.parseInt(phoneString);
-                System.out.println("we can now add some info");
-                int in_memberlevel = 1;
-                // boolean isAddUserOK = dbAccess.InsertNewUser(fName, lName, in_memberlevel, email, phone, userName, password);
+              int phone = Integer.parseInt(phoneString);
+              System.out.println("we can now add some info");
+              int in_memberlevel = 1;
+              BikeUser newUser = new BikeUser(
+                  fName, lName, in_memberlevel, email, phone, userName, password);
+              isAddUserOK = serverCall.createNewUser(newUser);
+            }
                 if (!isAddUserOK) {
-                    ErrorView.showError(errorTitle, "fel vid inläsning", "Kontrollera era uppgifter", userID, new IOException(" :-( kunde inte lägga till användare"));
+                    ErrorView.showError(errorTitle, "fel vid inläsning", "Kontrollera era uppgifter", userID, new Exception(" :-( kunde inte lägga till användare"));
                 }
                 if (isAddUserOK) {
-                    boolean d = DialogView.showSimpleInfo("Ny användare upplaggd", "Lyckades", "Ny användare är nu upplagd");
+                    //boolean d = DialogView.showSimpleInfo("Ny användare upplaggd", "Lyckades", "Ny användare är nu upplagd");
                     Main.getSpider().getMain().showLoginView();
                 }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            ErrorView.showError(errorTitle, "fel vid lägg till användare", "Kontrollera era uppgifter", userID, e);
-        }*/
             }
 
 
